c370b97e603b0f8da9e6375e1682f7a03faaf35c
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-02 18:43:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,12 +3,10 @@ def reverse_each_word (string)
   
   string = string.split(" ")
   new_sentence = []
-  
   string.each do |element| 
-    new_sentence.unshift(element)
+  new_sentence.unshift(element)
   end
   
   return new_sentence.join(" ")
-
   
 end
