8a8da0bacbf317e6edd4f47a9cc5e6413c6902f5
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-02 19:06:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,16 +2,5 @@
 def reverse_each_word (string)
   
   string = string.split(" ")
-  new = [] 
-  string.each do |element| 
-    word = element.split('')
-    wordArr = []
-    word.each do | letter |
-      wordArr.push(word.pop)
-    end
-    
-  new.push(wordArr.join)
-  end
-  new.join(" ")
-
+  
 end
